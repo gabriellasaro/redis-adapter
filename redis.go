4f48b2e2ffdc2c91d63e9710844daf0559890aff
Gabriel Lasaro <gabriel@lasaro.dev>
2024-04-11 11:58:56 -0300

    implementa adaptadores para os métodos (Get, GetInt64, GetBool, IncrBy, Delete, Close)

--- redis.go
+++ redis.go
@@ -16,6 +16,18 @@ func NewARedis[K ~string](client *redis.Client) *RedisAdapter[K] {
 	return &RedisAdapter[K]{client}
 }
 
+func (r *RedisAdapter[K]) Get(ctx context.Context, key K) (string, error) {
+	return r.client.Get(ctx, string(key)).Result()
+}
+
+func (r *RedisAdapter[K]) GetInt64(ctx context.Context, key K) (int64, error) {
+	return r.client.Get(ctx, string(key)).Int64()
+}
+
+func (r *RedisAdapter[K]) GetBool(ctx context.Context, key K) (bool, error) {
+	return r.client.Get(ctx, string(key)).Bool()
+}
+
 func (r *RedisAdapter[K]) GetJSON(ctx context.Context, key K, dest any) error {
 	data, err := r.client.Get(ctx, string(key)).Bytes()
 	if err != nil {
@@ -32,4 +44,16 @@ func (r *RedisAdapter[K]) SetJSON(ctx context.Context, key K, value any, exp tim
 	}
 
 	return r.client.Set(ctx, string(key), v, exp).Err()
+}
+
+func (r *RedisAdapter[K]) IncrBy(ctx context.Context, key K, val int64) error {
+	return r.client.IncrBy(ctx, string(key), val).Err()
+}
+
+func (r *RedisAdapter[K]) Delete(ctx context.Context, key K) error {
+	return r.client.Del(ctx, string(key)).Err()
+}
+
+func (r *RedisAdapter[K]) Close() error {
+	return r.client.Close()
 }
